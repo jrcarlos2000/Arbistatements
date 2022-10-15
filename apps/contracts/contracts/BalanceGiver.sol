676e90deac2738d73eee2fa3e367aa32684ef270
CarlosR <carlos.ramos@grupoimportia.com>
2022-10-15 13:52:59 -0500

    init contracts

--- apps/contracts/contracts/BalanceGiver.sol
+++ apps/contracts/contracts/BalanceGiver.sol
@@ -4,4 +4,22 @@ interface IERC20 {
 }
 contract BalanceGiver {
 
+    address[5] public supportedTokens;
+
+    constructor(address[5] memory tokensAddr) {
+
+        for(uint256 i=0;i<5;i++){
+            supportedTokens[i] = tokensAddr[i];
+        }
+    }
+
+    function generateBalances() external {
+        for(uint256 i=0;i<5;i++){
+            _generateBalances(supportedTokens[i],msg.sender);
+        }
+    }
+    function _generateBalances(address token, address user) internal {
+        IERC20(token).faucet(address(this));
+        IERC20(token).transfer(user,5e10);
+    }
 }
